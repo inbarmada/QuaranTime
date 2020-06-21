56213bbcdae131cfb8246d94859d7d5dfb667c0d
Inbar Lei <inbar.mada@gmail.com>
2020-06-21 12:01:21 -0500

    Make and display score

--- app/src/main/java/com/example/quarantime/MainActivity.java
+++ app/src/main/java/com/example/quarantime/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.quarantime;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -20,10 +22,20 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
+    public static int score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        score = getScore();
+        //Put score in preferences
+        //setting preferences
+        SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("score", score);
+        editor.commit();
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
@@ -32,5 +44,37 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         },SPLASH_TIME_OUT);
+    }
+
+    public void getScore(View view) {
+        SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
+        try {
+            int score = prefs.getInt("score", 0); //0 is the default value
+            Log.d("HomeActivity", "Getting score : " + score);
+        } catch (Exception e) {
+            Log.d("HomeActivity", "Getting score : " + 0 + " (zero)");
+        }
+    }
+
+    public int getScore() {
+        SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
+        try {
+            int score = prefs.getInt("score", 0); //0 is the default value
+            Log.d("HomeActivity", "Getting score : " + score);
+            return score;
+        } catch (Exception e) {
+            Log.d("HomeActivity", "Getting score : " + 0 + " (zero)");
+            return 0;
+        }
+    }
+
+    public  void addScore() {
+        Log.d("HomeActivity", "Putting score : ");
+        SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("score", score + 5);
+        editor.commit();
+        score += 5;
+        Log.d("HomeActivity", "Checking score : " + getScore());
     }
 }
