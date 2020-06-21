90f3f4bc2aba548735f0a86b2a5a50ba2dc8fe91
yuvi027 <yuvali027@gmail.com>
2020-06-20 21:49:56 -0500

    progress on loading page

--- app/src/main/java/com/example/quarantime/MainActivity.java
+++ app/src/main/java/com/example/quarantime/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.quarantime;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -15,60 +17,18 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-    private static int id = 0;
+    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
-
-
-    public void addUser(View view) {
-        //Debug
-        Log.d("addUser", "Added");
-
-
-        //Get username and password variables
-        EditText username   = (EditText)findViewById(R.id.username);
-        EditText userpassword   = (EditText)findViewById(R.id.username);
-        //Create database
-        DBHandler dbHandler = new DBHandler(this, null, null, 1);
-        //int id = Integer.parseInt(userid.getText().toString());
-        id++;
-        String name = username.getText().toString();
-        int password_hash = (userpassword.getText().toString()).hashCode();
-        Log.d("loadUsers", "Name:" + name + "  Password:"+password_hash);
-        User user = new User(id, name, password_hash);
-        dbHandler.addHandler(user);
-        username.setText("");
-        userpassword.setText("");
-    }
-
-
-    public void loadUsers(View view) {
-        //Debug
-        Log.d("loadUsers", "Loaded");
-
-        //Get username and password variables
-        EditText username   = (EditText)findViewById(R.id.username);
-        EditText userpassword   = (EditText)findViewById(R.id.username);
-        TextView show = (TextView)findViewById(R.id.show);
-        //Create database
-        DBHandler dbHandler = new DBHandler(this, null, null, 1);
-        show.setText(dbHandler.loadHandler());
-        username.setText("");
-        userpassword.setText("");
-
-    }
-
 }
