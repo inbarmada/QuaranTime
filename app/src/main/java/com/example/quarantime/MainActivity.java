8e272f419e4ba6ecf7103c4ab6c82f9657b65183
yuvi027 <yuvali027@gmail.com>
2020-06-21 11:07:22 -0500

    progress on popup

--- app/src/main/java/com/example/quarantime/MainActivity.java
+++ app/src/main/java/com/example/quarantime/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -24,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Pop.class));
+            }
+        });
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
@@ -33,4 +41,5 @@ public class MainActivity extends AppCompatActivity {
             }
         },SPLASH_TIME_OUT);
     }
+
 }
---
+++ app/src/main/java/com/example/quarantime/Pop.java
@@ -0,0 +1,8 @@
+package com.example.quarantime;
+
+import android.app.Activity;
+
+public class Pop extends Activity {
+    @Override
+    protected void onCreate
+}
