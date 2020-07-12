5b92787664c293e7f92b7cee47cf2ed2038a40eb
Inbar Lei <inbar.mada@gmail.com>
2020-07-12 14:57:06 -0500

    Create a recycler view which is scrollable and shows all the names of the tasks on the db

--- app/src/main/java/com/example/quarantime/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/quarantime/ui/dashboard/DashboardFragment.java
@@ -1,6 +1,7 @@
 package com.example.quarantime.ui.dashboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,25 +12,56 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.quarantime.R;
+import com.example.quarantime.Task;
+import com.example.quarantime.TaskDBHandler;
+
+import java.text.ParseException;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-//        final TextView textView = root.findViewById(R.id.text_dashboard);
-//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+
+        Log.d("Notes: DashFragment", "oncreateview created root");
+
+        // 1. get a reference to recyclerView
+        RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.recView);
+        recyclerView.setHasFixedSize(true);
+
+        Log.d("Notes: DashFragment", "oncreateview: created recView");
+
+        // 2. set layoutManger
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        Log.d("Notes: DashFragment", "oncreateview: set layout manager");
+
+        // 3. Get Data
+        TaskDBHandler taskDB = new TaskDBHandler(getContext(), null);
+        Task[] taskList = taskDB.getTasks();
+        Log.d("Notes: DashFragment", "oncreateview: get tasks");
+
+        for (int i = 0; i < taskList.length; i++) {
+            Log.d("Notes: DashFragment", "task name " + i + " " + taskList[i].getName());
+        }
+        // 4. create an adapter
+        MyAdapter mAdapter = new MyAdapter(taskList);
+        Log.d("Notes: DashFragment", "oncreateview: create adapter");
+        // 5. set adapter
+        recyclerView.setAdapter(mAdapter);
+        Log.d("Notes: DashFragment", "oncreateview: set adapter");
+
         return root;
     }
 }
---
+++ app/src/main/java/com/example/quarantime/ui/dashboard/MyAdapter.java
@@ -0,0 +1,67 @@
+package com.example.quarantime.ui.dashboard;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.quarantime.R;
+import com.example.quarantime.Task;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private Task[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView textView;
+        public MyViewHolder(TextView v) {
+            super(v);
+            textView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(Task[] myDataset) {
+        Log.d("Notes: MyAdapter", "constructor");
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+
+        Log.d("Notes: MyAdapter", "oncreateviewholder created textview");
+
+
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_view, parent, false);
+
+        Log.d("Notes: MyAdapter", "oncreateviewholder created textview");
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        Log.d("Notes: DashFragment", "onBindViewHolder");
+        holder.textView.setText(mDataset[position].getName());
+        Log.d("Notes: DashFragment", "onBindViewHolder done");
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
