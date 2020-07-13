cf46d1480b7afd5a304dd205c6fa2a4afa83300d
yuvi027 <yuvali027@gmail.com>
2020-07-12 20:17:13 -0500

    working on popup window

--- app/src/main/java/com/example/quarantime/Pop.java
+++ app/src/main/java/com/example/quarantime/Pop.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.util.Date;
 
 public class Pop extends Activity {
+    private EditText edittext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +24,13 @@ public class Pop extends Activity {
         getWindowManager().getDefaultDisplay().getMetrics(dm);
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+        getWindow().setLayout((int)(width*.8),(int)(height*.8));
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.CENTER;
         params.x = 0;
         params.y = -20;
         getWindow().setAttributes(params);
+        addKeyListener();
     }
 
     public void addTaskButton(View view) throws ParseException {
@@ -37,5 +42,33 @@ public class Pop extends Activity {
         Task t = new Task(-1, "Homework", "Tough", "2020-07-12 12:12:12", false, "school", 15);
 
         dbHandler.addHandler(t);
+    }
+    public void addKeyListener() {
+
+        // get edittext component
+        edittext = (EditText) findViewById(R.id.editTextTextDescription);
+
+        // add a keylistener to monitor the keaybord avitvity...
+        edittext.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                // if the users pressed a button and that button was "0"
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_0)) {
+
+                    // display the input text....
+                    Toast.makeText(Pop.this,edittext.getText(), Toast.LENGTH_LONG).show();
+                    return true;
+
+                    // if the users pressed a button and that button was "9"
+                } else if ((event.getAction() == KeyEvent.ACTION_DOWN)  && (keyCode == KeyEvent.KEYCODE_9)) {
+
+                    // display message
+                    Toast.makeText(Pop.this, "Number 9 is pressed!", Toast.LENGTH_LONG).show();
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
