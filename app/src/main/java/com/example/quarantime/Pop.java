c2d3c369006935966257e128e3b6b9b5e9931b07
Inbar Lei <inbar.mada@gmail.com>
2020-07-12 21:59:47 -0500

    Can add tasks to the task list

--- app/src/main/java/com/example/quarantime/Pop.java
+++ app/src/main/java/com/example/quarantime/Pop.java
@@ -9,6 +9,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -22,7 +23,7 @@ public class Pop extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popwindow);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Spinner spinner = (Spinner) findViewById(R.id.taskCatSP);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.categories_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -48,15 +49,22 @@ public class Pop extends Activity {
         Log.d("Notes: pop", "adding task");
 
         TaskDBHandler dbHandler = new TaskDBHandler(this, null);
-        //Date d =
-        Task t = new Task(-1, "Homework", "Tough", "2020-07-12 12:12:12", false, "school", 15);
+        String name = ((EditText)findViewById(R.id.taskNameET)).getText().toString();
+        String desc = ((EditText)findViewById(R.id.taskDescET)).getText().toString();
+        String date = ((EditText)findViewById(R.id.taskDateET)).getText().toString();
+        String cat = ((Spinner)findViewById(R.id.taskCatSP)).getSelectedItem().toString();
+        boolean rem = ((CheckBox)findViewById(R.id.taskRemCB)).isChecked();
+        int score = Integer.parseInt(((EditText)findViewById(R.id.taskScoreET)).getText().toString());
+
+        Task t = new Task(-1, name, desc, date, rem, cat, score);
 
         dbHandler.addHandler(t);
     }
+
     public void addKeyListener() {
 
         // get edittext component
-        edittext = (EditText) findViewById(R.id.editTextTextDescription);
+        edittext = (EditText) findViewById(R.id.taskDescET);
 
         // add a keylistener to monitor the keaybord avitvity...
         edittext.setOnKeyListener(new View.OnKeyListener() {
